6d33c3e2ac9327be2320b4d469b34d216d67e986
gusty <gustavodiorlando@gmail.com>
2023-05-30 14:30:11 -0300

    Change variables with button clicks

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function divide(a, b) {
     return a / b;
 }
 
-let firstNumber, operator, secondNumber;
+let firstNumber = ''; 
+let secondNumber = '';
+let operator = '';
 
 function operate(firstNumber, operator, secondNumber) {
     return operator === "+" ? add(firstNumber, secondNumber)
@@ -22,4 +24,28 @@ function operate(firstNumber, operator, secondNumber) {
          : operator === '*' ? multiply(firstNumber, secondNumber)
          : operator === '/' ? divide(firstNumber, secondNumber)
          : 'error';
-} 
+}
+
+const numberButtons = document.querySelectorAll('button.number');
+const operatorButtons = document.querySelectorAll('button.operator')
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (operator === '') {
+            firstNumber += button.id;
+            console.log(firstNumber);
+        } else {
+            secondNumber += button.id;
+            console.log(secondNumber);
+        }
+    })
+})
+
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (operator === '') {
+            operator += button.id;
+            console.log(operator);
+        }
+    })
+})
