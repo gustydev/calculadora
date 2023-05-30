fc635db5f9b184e0a2e264221105bdd9bbf37c31
gusty <gustavodiorlando@gmail.com>
2023-05-30 13:42:51 -0300

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function multiply(a,b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNumber, operator, secondNumber;
+
+function operate(firstNumber, operator, secondNumber) {
+    return operator === "+" ? add(firstNumber, secondNumber)
+         : operator === '-' ? subtract(firstNumber, secondNumber)
+         : operator === '*' ? multiply(firstNumber, secondNumber)
+         : operator === '/' ? divide(firstNumber, secondNumber)
+         : 'error';
+} 
